179f978793a377b6c0143cffcf33c347aed85d7c
Ashley Custer <ashleycuster@Ashleys-MacBook-Pro-2.local>
2023-08-31 14:14:21 -0700

    homepage: hover and active styles

--- homepage.js
+++ homepage.js
@@ -1,11 +1,27 @@
 const headerNavOffset = 60;
 const projectsYesButton = document.getElementById("projects-yes");
-const scrollToProjects = (event) => {
+const projectsNoButton = document.getElementById("projects-no");
+const scrollToProjects = () => {
     const projectsYOffset = document.getElementById("projects-directory").offsetTop;
     window.scroll({
         top: projectsYOffset - headerNavOffset,
         behavior: "smooth",
     });
 };
+const toggleActiveButton = (activeButton, inactiveButton) => {
+    inactiveButton.classList.remove("active");
+    activeButton.classList.add("active");
+};
 
-projectsYesButton.addEventListener("click", scrollToProjects);
+const onProjectsYesClick = (event) => {
+    toggleActiveButton(projectsYesButton, projectsNoButton);
+    scrollToProjects();
+};
+
+const onProjectsNoClick = (event) => {
+    toggleActiveButton(projectsNoButton, projectsYesButton);
+    // show popup
+};
+
+projectsYesButton.addEventListener("click", onProjectsYesClick);
+projectsNoButton.addEventListener("click", onProjectsNoClick);
