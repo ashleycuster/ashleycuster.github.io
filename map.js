69ac3558bea938bbc0ced4080c5454bc5ea1fb65
Ashley Custer <ashleycuster@Ashleys-MBP-2.attlocal.net>
2023-10-29 13:44:55 -0700

    some cleanup

--- map.js
+++ map.js
@@ -1,84 +1,41 @@
 import {STATE_COORDINATES} from "./constants.js";
 
 // TODO: 
-// UPDATE COLORS 
 // CLEAN UP CODE
-// UPDATE CHARITY NAME TO SOMETHING FUN
-// DONATIONS LOADING MESSAGE
-// DONATION ANIMATION
-// update tag icon to have thicker lines
 // tweak map centering if there is time
 // UPDATE MAP TOOLTIPS TO SHOW ON TAP AS WELL AS HOVER (FOR MOBILE)
 
-
 const numberFormatter = new Intl.NumberFormat('en-US'); 
 
+const addDonateButtonClickEvent = () => {
+    document.getElementById("donate-button")
+        .addEventListener("click", () => {
+            const donationCircle = document.getElementById("donation-circle-Washington");
+            donationCircle.classList.add("pulse");
+            setTimeout(() => {
+                donationCircle.classList.remove("pulse");
+            }, 3000);
+        });
+}
 
-const renderMap = async () => {
-    // let world = await fetch("land-110m.json");
-    let countries = await fetch("https://unpkg.com/world-atlas@1.1.4/world/110m.json");
-    countries = await countries.json();
-
-    const { select, geoPath, geoNaturalEarth1 } = d3;
-    const svg = select('svg.map');
-    const projection = geoNaturalEarth1()
-        .center([-105,39.5])
-        .scale(750);
-    const pathGenerator = geoPath().projection(projection);
-    svg.append('path')
-        .attr('class', 'sphere')
-        .attr('d', pathGenerator({type: 'Sphere'}));
-    
-    const countriesData = topojson.feature(countries, countries.objects.countries);
-    svg.selectAll('path').data(countriesData.features)
-        .enter().append('path')
-        .attr('class', 'country')
-        .attr('d', pathGenerator);
-
-    const charityData = [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-77.0369,38.9072]
-            }
-        }
-    ];
-
-    svg.selectAll('.charity')
-        .data(charityData)
-        .enter()
-        .append('image')
-        .attr('xlink:href', "marker.svg")
-        .attr('d', pathGenerator)
-        // .style("left", projection([-77.0369,38.9072])[0])
-        // .style("top", projection([-77.0369,38.9072])[1])
-        .attr("x", projection([-77.0369,38.9072])[0] - 12)
-        .attr("y", projection([-77.0369,38.9072])[1] - 12)
-        .attr("width", 24)
-        .attr("height", 24)
-        .attr('class', 'charity');
-
-    const stateCensusResponse = await fetch("https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=state:*");
-    const stateCensusData = await stateCensusResponse.json();
-
-    const getStateLatitude = stateName => {
-        if (!STATE_COORDINATES.hasOwnProperty(stateName)) {
-            return null;
-        }
-        return STATE_COORDINATES[stateName].lat;
+const getStateLatitude = stateName => {
+    if (!STATE_COORDINATES.hasOwnProperty(stateName)) {
+        return null;
     }
+    return STATE_COORDINATES[stateName].lat;
+}
 
-    const getStateLongitude = stateName => {
-        if (!STATE_COORDINATES.hasOwnProperty(stateName)) {
-            return null;
-        }
-        return STATE_COORDINATES[stateName].lon;
+const getStateLongitude = stateName => {
+    if (!STATE_COORDINATES.hasOwnProperty(stateName)) {
+        return null;
     }
+    return STATE_COORDINATES[stateName].lon;
+}
 
+const getStateData = (stateCensusData) => {
     let minimumPopulation = 9000000000;
     let maximumPopulation = 0;
-    const donationData2 = [];
+    const donationData = [];
 
     stateCensusData.forEach((state, index) => {
         // first item defines structure of data
@@ -94,7 +51,7 @@ const renderMap = async () => {
         if (population < minimumPopulation) {
             minimumPopulation = population;
         }
-        donationData2.push({
+        donationData.push({
             stateName: name,
             population,
             lat: getStateLatitude(name),
@@ -103,12 +60,72 @@ const renderMap = async () => {
         return;
     });
 
+    return {minimumPopulation, maximumPopulation, donationData};
+}
+
+function onDonationDotMouseover(d) {
+    const tooltip = document.getElementById("tooltip-donation-dot");
+    const tooltipStateName = document.getElementById("tooltip-state-name");
+    const tooltipDonationValue = document.getElementById("tooltip-donation-amount-value");
+    const { left, top } = this.getBoundingClientRect();
+    const windowWidth = window.innerWidth;
+    const leftOffset = left > windowWidth/2 ? -160 : 12;
+
+    tooltipStateName.textContent = d.stateName;
+    tooltipDonationValue.textContent = numberFormatter.format(d.population);
+    tooltip.style.left = left + leftOffset;
+    tooltip.style.top = top + 24;
+    tooltip.classList.add("show");
+}
+
+const onDonationDotMouseout = (d) => {
+    const tooltip = document.getElementById("tooltip-donation-dot");
+            
+    tooltip.classList.remove("show");
+}
+
+const renderMap = async () => {
+    addDonateButtonClickEvent();
+    // let world = await fetch("land-110m.json");
+    let countries = await fetch("https://unpkg.com/world-atlas@1.1.4/world/110m.json");
+    countries = await countries.json();
+
+    const { select, geoPath, geoNaturalEarth1 } = d3;
+    const svg = select('svg.map');
+    const projection = geoNaturalEarth1()
+        .center([-105,39.5])
+        .scale(750);
+    const pathGenerator = geoPath().projection(projection);
+    const countriesData = topojson.feature(countries, countries.objects.countries);
+
+    svg.append('path')
+        .attr('class', 'sphere')
+        .attr('d', pathGenerator({type: 'Sphere'}));
+    
+    svg.selectAll('path').data(countriesData.features)
+        .enter().append('path')
+        .attr('class', 'country')
+        .attr('d', pathGenerator);
+
+    svg.append('image')
+        .attr('xlink:href', "marker.svg")
+        .attr('d', pathGenerator)
+        .attr("x", projection([-121.4944,38.5816])[0] - 24)
+        .attr("y", projection([-121.4944,38.5816])[1] - 24)
+        .attr("width", 48)
+        .attr("height", 48)
+        .attr('class', 'charity');
+
+    const stateCensusResponse = await fetch("https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=state:*");
+    const stateCensusData = await stateCensusResponse.json();
+    const {minimumPopulation, maximumPopulation, donationData} = getStateData(stateCensusData);
+
     const radius = d3.scaleSqrt()
         .domain([minimumPopulation, maximumPopulation])
         .range([2,26]);
     
     svg.selectAll("circle")
-        .data(donationData2)
+        .data(donationData)
         .enter()
         .append("circle")
         .attr("cx", d=>projection([d.lon,d.lat])[0])
@@ -116,25 +133,11 @@ const renderMap = async () => {
         .attr("r", d=> radius(d.population))
         .attr("class", "donation-circles")
         .attr("id", d => `donation-circle-${d.stateName}`)
-        .on("mouseover", function(d) { 
-            const tooltip = document.getElementById("tooltip-donation-dot");
-            const tooltipStateName = document.getElementById("tooltip-state-name");
-            const tooltipDonationValue = document.getElementById("tooltip-donation-amount-value");
-            const { left, top } = this.getBoundingClientRect();
-            const windowWidth = window.innerWidth;
-            const leftOffset = left > windowWidth/2 ? -160 : 12;
+        .on("mouseover", onDonationDotMouseover)
+        .on("mouseout", onDonationDotMouseout);
 
-            tooltipStateName.textContent = d.stateName;
-            tooltipDonationValue.textContent = numberFormatter.format(d.population);
-            tooltip.style.left = left + leftOffset;
-            tooltip.style.top = top + 24;
-            tooltip.classList.add("show");
-        })
-        .on("mouseout", function(d) { 
-            const tooltip = document.getElementById("tooltip-donation-dot");
-
-            tooltip.classList.remove("show");
-        });
+    document.getElementById("loading-donations")
+        .classList.add("hide");
   }
 
   renderMap();
